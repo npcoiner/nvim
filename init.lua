7d2f164f3d5e621d5e02dd99522547ef52333b9b
npcoiner <nicholascoiner@gmail.com>
2026-04-04 12:31:18 -0700

    Added neovim basic options

--- init.lua
+++ init.lua
@@ -5,3 +5,27 @@ vim.pack.add({
 	gh('nvim-treesitter/nvim-treesitter'),
 	gh('neovim/nvim-lspconfig'),
 })
+
+-- options
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+
+vim.opt.tabstop = 2 -- tabwidth
+vim.opt.shiftwidth = 2 -- indentwidth
+vim.opt.expandtab = true -- use spaces for tab (idk what this means tbh lol)
+vim.opt.smartindent = true -- smart auto-indenting
+vim.opt.autoindent = true -- copies indent of current line on newline
+
+vim.opt.ignorecase = true -- ignore casing
+vim.opt.smartcase = true -- don't ignore casing if uppercase in string
+vim.opt.hlsearch = true -- highlight search matches
+vim.opt.incsearch = true -- shows search match as being typed
+
+vim.opt.hidden = true -- allow hidden buffers
+vim.opt.errorbells = false -- no term bell on error
+
+vim.opt.encoding = "utf-8" -- default encoding
