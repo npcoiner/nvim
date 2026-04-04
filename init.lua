f063590e316df009fb539906cd3543c09d55bc3e
npcoiner <nicholascoiner@gmail.com>
2026-04-04 16:58:17 -0700

    Added LSP linter and formatter

--- init.lua
+++ init.lua
@@ -4,24 +4,26 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.wrap = true
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 100
 vim.opt.sidescrolloff = 10
 
-vim.opt.tabstop = 2 -- tabwidth
-vim.opt.shiftwidth = 2 -- indentwidth
-vim.opt.expandtab = true -- use spaces for tab (idk what this means tbh lol)
+vim.opt.tabstop = 2        -- tabwidth
+vim.opt.shiftwidth = 2     -- indentwidth
+vim.opt.expandtab = true   -- use spaces for tab (idk what this means tbh lol)
 vim.opt.smartindent = true -- smart auto-indenting
-vim.opt.autoindent = true -- copies indent of current line on newline
+vim.opt.autoindent = true  -- copies indent of current line on newline
 
-vim.opt.ignorecase = true -- ignore casing
-vim.opt.smartcase = true -- don't ignore casing if uppercase in string
-vim.opt.hlsearch = true -- highlight search matches
-vim.opt.incsearch = true -- shows search match as being typed
+vim.opt.ignorecase = true  -- ignore casing
+vim.opt.smartcase = true   -- don't ignore casing if uppercase in string
+vim.opt.hlsearch = true    -- highlight search matches
+vim.opt.incsearch = true   -- shows search match as being typed
 
-vim.opt.hidden = true -- allow hidden buffers
+vim.opt.hidden = true      -- allow hidden buffers
 vim.opt.errorbells = false -- no term bell on error
 
 vim.opt.encoding = "utf-8" -- default encoding
+
+vim.opt.termguicolors = true
 
 -- keymap
 vim.g.mapleader = " "
@@ -43,7 +45,7 @@ vim.keymap.set('v', '<C-S-c>', '"+y', { noremap = true })
 
 --highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("UserConfig", {clear = true}),
+  group = vim.api.nvim_create_augroup("UserConfig", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -61,11 +63,16 @@ vim.pack.add({
     branch = "main",
     build = ":TSUpdate",
   },
-	gh('neovim/nvim-lspconfig'),
-	gh('nvim-tree/nvim-tree.lua'),
+  gh('neovim/nvim-lspconfig'),
+  gh('nvim-tree/nvim-tree.lua'),
   gh('ibhagwan/fzf-lua'),
   gh('echasnovski/mini.nvim'),
   gh('lewis6991/gitsigns.nvim'),
+  gh('mason-org/mason.nvim'),
+  gh('mason-org/mason-lspconfig.nvim'),
+  gh('stevearc/conform.nvim'),
+  gh('mfussenegger/nvim-lint'),
+  gh('WhoIsSethDaniel/mason-tool-installer.nvim'),
 })
 
 --nvim-tree
@@ -82,16 +89,16 @@ require("nvim-tree").setup({
 })
 vim.keymap.set("n", "<leader>ex", function()
   require("nvim-tree.api").tree.toggle()
-end, {desc = "Toggle NvimTree"})
+end, { desc = "Toggle NvimTree" })
 
 --fzf-lua
 require("fzf-lua").setup({})
 vim.keymap.set("n", "<leader>ff", function()
   require("fzf-lua").files()
-end, {desc = "FZF files"})
+end, { desc = "FZF files" })
 vim.keymap.set("n", "<leader>fg", function()
   require("fzf-lua").live_grep()
-end, {desc = "Live Grep"})
+end, { desc = "Live Grep" })
 
 --mini.nvim
 require("mini.comment").setup({})
@@ -104,50 +111,92 @@ require("gitsigns").setup({})
 
 --nvim-treesitter
 local setup_treesitter = function()
-	local treesitter = require("nvim-treesitter")
-	treesitter.setup({})
-	local ensure_installed = {
-		"go",
+  local treesitter = require("nvim-treesitter")
+  treesitter.setup({})
+  local ensure_installed = {
+    "go",
     "lua",
     "python",
     "typescript",
     "vue",
-		"html",
-		"css",
-		"javascript",
+    "html",
+    "css",
+    "javascript",
     "bash",
     "c_sharp",
     "zig",
     "rust",
     "c",
-		"json",
-		"svelte",
-	}
+    "json",
+    "svelte",
+  }
 
-	local config = require("nvim-treesitter.config")
+  local config = require("nvim-treesitter.config")
 
-	local already_installed = config.get_installed()
-	local parsers_to_install = {}
+  local already_installed = config.get_installed()
+  local parsers_to_install = {}
 
-	for _, parser in ipairs(ensure_installed) do
-		if not vim.tbl_contains(already_installed, parser) then
-			table.insert(parsers_to_install, parser)
-		end
-	end
+  for _, parser in ipairs(ensure_installed) do
+    if not vim.tbl_contains(already_installed, parser) then
+      table.insert(parsers_to_install, parser)
+    end
+  end
 
-	if #parsers_to_install > 0 then
-		treesitter.install(parsers_to_install)
-	end
+  if #parsers_to_install > 0 then
+    treesitter.install(parsers_to_install)
+  end
 
-	local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
-	vim.api.nvim_create_autocmd("FileType", {
-		group = group,
-		callback = function(args)
-			if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
-				vim.treesitter.start(args.buf)
-			end
-		end,
-	})
+  local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+        vim.treesitter.start(args.buf)
+      end
+    end,
+  })
 end
-
 setup_treesitter()
+
+--mason + mason-lspconfig
+require("mason").setup({})
+require("mason-lspconfig").setup({})
+
+-- lsp
+vim.keymap.set("n", "<leader>q", function()
+  vim.diagnostic.setloclist({ open = true })
+end, { desc = "Open diag list" })
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+      telemetry = { enable = false },
+    },
+  },
+})
+
+--mason-tool-installer
+require('mason-tool-installer').setup({
+  ensure_installed = {
+    'prettier',
+    'eslint_d',
+    'csharpier',
+  },
+})
+
+--formatter
+require('conform').setup({
+  formatters_by_ft = {
+    javascript = { "prettier" },
+    cs = { "csharpier" },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+})
+
+--linter
+require('lint').linters_by_ft = {
+  javascript = { 'eslint_d' }
+}
