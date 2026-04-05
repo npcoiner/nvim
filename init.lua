d4d057173584facf99b09ebff54b768f62dd0a00
npcoiner <nicholascoiner@gmail.com>
2026-04-05 00:13:36 -0700

    Turned on mini completion for now

--- init.lua
+++ init.lua
@@ -111,6 +111,7 @@ require("mini.comment").setup({})
 require("mini.trailspace").setup({})
 require("mini.icons").setup({})
 require("mini.indentscope").setup({})
+require("mini.completion").setup({})
 
 --git signs
 require("gitsigns").setup({})
